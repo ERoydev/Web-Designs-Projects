cf9d9dba379987a7ee39090a62458a2762f55637
Emil <e.roydev@gmail.com>
2024-05-29 00:50:42 +0300

    latests

---
+++ Portfolio-Tailwind/src/App.jsx
@@ -0,0 +1,36 @@
+export default function App() {
+  return (
+    <main className="relative">
+      Nav
+      <section className="xl:padding-1
+      wide:padding-r padding-b">
+        Hero
+      </section>
+
+      <section className="padding">
+        About
+      </section>
+
+      <section className="padding bg-pale-blue">
+        Projects
+      </section>
+
+      <section className="padding-x sm:py-32 py-16 w-full">
+        Work experience
+      </section>
+
+      <seciton className="padding">
+        Approach
+      </seciton>
+
+      <section className="padding">
+        Education
+      </section>
+
+      <section className="padding">
+        Contact
+      </section>
+
+    </main>
+  )
+}
