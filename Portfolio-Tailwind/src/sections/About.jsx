36a2f1f928dfcabc1434fb8abf45e73183278276
Emil <e.roydev@gmail.com>
2024-06-01 05:27:34 +0300

    updates

--- Portfolio-Tailwind/src/sections/About.jsx
+++ Portfolio-Tailwind/src/sections/About.jsx
@@ -1,6 +1,41 @@
+import computerImg from "../assets/computer.jpg";
+
 
 export default function About(){
+
     return(
-        <div>About</div>
+        <section className="flex flex-col items-center w-full .max-container">
+
+            <div className="relative xl:w-4/5 grid md:grid-cols-3 auto-rows-[250px] gap-7">
+                
+                <div className="md:col-span-2 md:row-span-2 window flex">
+                    <div className="overflow-hidden rounded-3xl w-full relative">
+                        <img src={computerImg} alt="Computer" className="w-full h-full object-cover"/>
+                        <div class="absolute inset-0 bg-black opacity-80 shadow-inner"></div>
+                        <h1 className="absolute z-20 md:bottom-10 bottom-5 sm:px-10 px-5 opacity-100 main-text lg:text-[1.5rem] sm:text-[0.9rem] text-[0.8rem]">I view my relationship with clients as a partnership, where their goals become my own.</h1>
+                    </div>
+                </div>
+
+                <div className="window relative">
+                    <h1 className="absolute z-20 md:bottom-10 bottom-5 sm:px-10 px-5 opacity-100 main-text lg:text-[1.5rem] sm:text-[0.9rem] text-[0.8rem]">I maintain transparency, offer updates and seek feedback for alignment and satisfaction.</h1>
+                </div>
+
+                <div className="window">
+                    <h1>I prioritize clients.</h1>
+                </div>
+
+                <div className="window">
+                    <h1>I prioritize clients.</h1>
+                </div>
+
+                <div className="row-start-4 window">
+                    <h1>I prioritize clients.</h1>
+                </div>
+
+                <div className="md:col-span-2 md:row-span-2 window">
+                    <h1>I prioritize clients.</h1>
+                </div>
+            </div>
+        </section>
     );
 }
--- Portfolio-Tailwind/tailwind.config.js
+++ Portfolio-Tailwind/tailwind.config.js
@@ -30,8 +30,10 @@ export default {
         '3xl': '0 10px 40px rgba(0, 0, 0, 0.1)'
       },
       screens: {
-        "wide": "1440px"
-      }
+        "wide": "1440px",
+        "large-custom": "1700px",
+        "md-custom": "1350px",
+      },
     },
   },
   plugins: [],
